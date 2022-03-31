239045d92987eb859f24620475ef59f12de98344
Teslim Olunlade <tolunlade@outlook.com>
2022-03-31 17:00:12 -0400

    Added register api call

--- app/src/main/java/io/invest/app/net/Investio.kt
+++ app/src/main/java/io/invest/app/net/Investio.kt
@@ -3,10 +3,8 @@ package io.invest.app.net
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.FormBody
-import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,6 +16,43 @@ class Investio @Inject constructor(private val client: OkHttpClient) {
         val url = "http://$BASE_URL/auth/login"
 
         val body = FormBody.Builder()
+            .add("username", username)
+            .add("password", password)
+            .build()
+
+        val req = Request.Builder().url(url).post(body).build()
+
+        return withContext(Dispatchers.IO) {
+            val res = client.newCall(req).execute()
+
+            if (res.code != 200) {
+                res.body?.close()
+                return@withContext null
+            }
+
+            val body = res.body?.string()
+
+            res.body?.close()
+
+            body
+        }
+    }
+
+    suspend fun register(
+        name: String,
+        phone: String,
+        email: String,
+        dob: Int,
+        username: String,
+        password: String
+    ): String? {
+        val url = "http://$BASE_URL/auth/register"
+
+        val body = FormBody.Builder()
+            .add("name", name)
+            .add("phone", phone)
+            .add("email", email)
+            .add("dob", dob.toString())
             .add("username", username)
             .add("password", password)
             .build()
