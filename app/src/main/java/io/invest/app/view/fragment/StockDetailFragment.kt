a466cb16d45b753324cdd44a982f052c413279b3
Teslim Olunlade <tolunlade@outlook.com>
2022-04-07 18:12:49 -0400

    Updated colors for candlestick

--- app/src/main/java/io/invest/app/view/fragment/StockDetailFragment.kt
+++ app/src/main/java/io/invest/app/view/fragment/StockDetailFragment.kt
@@ -1,5 +1,6 @@
 package io.invest.app.view.fragment
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,7 +13,9 @@ import androidx.navigation.fragment.navArgs
 import com.github.mikephil.charting.data.CandleData
 import com.github.mikephil.charting.data.CandleDataSet
 import com.github.mikephil.charting.data.CandleEntry
+import com.google.android.material.color.MaterialColors
 import dagger.hilt.android.AndroidEntryPoint
+import io.invest.app.R
 import io.invest.app.databinding.FragmentStockDetailBinding
 import io.invest.app.net.Investio
 import kotlinx.coroutines.launch
@@ -42,22 +45,39 @@ class StockDetailFragment : Fragment() {
 
             axisLeft.apply {
                 setDrawGridLines(false)
+                setDrawLabels(false)
+                isEnabled = false
             }
 
             axisRight.apply {
                 setDrawGridLines(false)
+                setDrawLabels(false)
+                isEnabled = false
             }
 
             xAxis.apply {
                 setDrawGridLines(false)
                 setDrawLabels(false)
-                granularity = 1f
-                isGranularityEnabled = true
                 setAvoidFirstLastClipping(true)
             }
 
+            setBorderColor(
+                MaterialColors.getColor(
+                    this,
+                    com.google.android.material.R.attr.colorOnSurface
+                )
+            )
+
+            setBackgroundColor(
+                MaterialColors.getColor(
+                    this,
+                    com.google.android.material.R.attr.colorSurface
+                )
+            )
+
             isDragEnabled = false
             setScaleEnabled(false)
+            description.isEnabled = false
             isDoubleTapToZoomEnabled = false
             legend.isEnabled = false
         }
@@ -67,7 +87,6 @@ class StockDetailFragment : Fragment() {
 
             res?.prices?.let {
                 val entries = it.mapIndexed { index, price ->
-                    Log.d(TAG, index.toString())
                     CandleEntry(
                         index.toFloat(),
                         price.high,
@@ -75,13 +94,38 @@ class StockDetailFragment : Fragment() {
                         price.open,
                         price.close
                     )
-                }
+                } as MutableList
+
+//                if (entries.size < 5) {
+//                    entries.addAll(
+//                        (entries.size..4).map { index ->
+//                            CandleEntry(index.toFloat(), 0f, 0f, 0f, 0f)
+//                        })
+//                }
 
                 val dataSet = CandleDataSet(entries, "")
 
+                dataSet.decreasingColor = MaterialColors.getColor(
+                    binding.chart,
+                    com.google.android.material.R.attr.colorError
+                )
+
+                dataSet.neutralColor = MaterialColors.getColor(
+                    binding.chart,
+                    com.google.android.material.R.attr.colorOnSurface
+                )
+
+                dataSet.increasingColor =
+                    MaterialColors.getColor(binding.chart, R.attr.colorSuccess)
+
+                dataSet.increasingPaintStyle = Paint.Style.FILL
+
+                dataSet.shadowColor = dataSet.neutralColor
+
                 binding.chart.data = CandleData(dataSet)
-                binding.chart.invalidate()
                 binding.chart.fitScreen()
+                binding.chart.moveViewToX(5f)
+                binding.chart.invalidate()
             }
 
             Log.d(TAG, res?.prices.toString())
